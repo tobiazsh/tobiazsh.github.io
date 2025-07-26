33deee135924369d30a8b27d807bdfef25fc54b3
Tobias <tobias.schweighofer0809@gmail.com>
2025-07-26 12:54:23 +0200

    Fix css name

--- app/components/socialMediaFloater.tsx
+++ app/components/socialMediaFloater.tsx
@@ -11,7 +11,7 @@ export default function SocialMediaFloater() {
 
 			<Divider orientation={"horizontal"} flexItem variant={"middle"} style={{backgroundColor: "rgba(var(--shine-color-raw), 0.5)", height: 1}}/>
 
-			<div className={`flex max-sm:flex-col space-x-10 max-sm:space-y-3 max-sm:pt-3 p-5 pb-3 text-xl text-center justify-center items-center max-sm:items-start ${styles.center_content_span}`}>
+			<div className={`flex max-sm:flex-col space-x-10 max-sm:space-y-3 max-sm:pt-3 p-5 pb-3 text-xl text-center justify-center items-center max-sm:items-start ${styles.center_content_a}`}>
 				<a href={"https://www.github.com/tobiazsh"}><FaGithub /> GitHub</a>
 				<a href={"https://www.instagram.com/tobizsh"}><FaInstagram /> Instagram</a>
 				<a href={"https://www.youtube.com/@tobiazsh"}><FaYoutube /> YouTube</a>
