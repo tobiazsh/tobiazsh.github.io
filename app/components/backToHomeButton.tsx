51c4531c9f0c528aa0a7c1b9d9e72a505a117b39
Tobias <51285655+tobiazsh@users.noreply.github.com>
2025-07-27 00:23:13 +0200

    Fix home button overlapping

--- app/components/backToHomeButton.tsx
+++ app/components/backToHomeButton.tsx
@@ -4,7 +4,7 @@ import {FaArrowLeft} from "react-icons/fa";
 
 export default function BackToHomeButton() {
 	return (
-		<div className={`top-0 fixed left-0 p-5`}>
+		<div className={`top-0 absolute left-0 p-5`}>
 			<Link href={"/"} className={`${styles.floating} ${styles.highlight_border} p-5 flex items-center`}>
 				<FaArrowLeft className={`mr-3`}/>
 				Back to Home
