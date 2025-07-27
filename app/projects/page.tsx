72ff4ff6d84f861a0efea4b9972ed50416963e10
Tobias <51285655+tobiazsh@users.noreply.github.com>
2025-07-27 14:53:58 +0200

    Add real project

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -14,9 +14,16 @@ export default function ProjectsPage() {
 				<div className={`flex flex-wrap md:w-4/5 w-full gap-5 justify-center`}>
 					<ProjectsPane
 						uuid={"67d23663-c20a-40ce-8d91-c626112eb1d3"}
-						name={"Some Project"}
-						description={"Hello, this is the description of some project"}
-						createdAt={new Date(Date.now())}
+						name={"MyWorld Traffic Addition"}
+						description={"A Minecraft Mod that adds normal and completely customizable traffic signs as well as other traffic utilities to the game."}
+						createdAt={new Date("2024-08-13")}
+					/>
+
+					<ProjectsPane
+						uuid={"a8df4cfc-99b7-410d-a94c-e167c8118ab8"}
+						name={"Jengua"}
+						description={"A Java language library that provides a simple and intuitive way to translate your Java application into multiple languages."}
+						createdAt={new Date("2025-06-25")}
 					/>
 				</div>
 			</div>
