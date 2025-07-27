b777a90d103d6a2a9af755c36ff383623d0290b8
Tobias <51285655+tobiazsh@users.noreply.github.com>
2025-07-27 14:54:34 +0200

    Add custom not found

---
+++ app/not-found.tsx
@@ -0,0 +1,23 @@
+import BackToHomeButton from "@/app/components/backToHomeButton";
+import Image from "next/image";
+
+export default function NotFound() {
+    return (
+        <div className={`w-[100dvw] h-[100dvh]`}>
+            <BackToHomeButton />
+
+            <div className={`w-full h-full flex justify-center items-center gap-20 max-md:flex-col`}>
+                <div className={`h-[20rem] aspect-[1451/1200] relative flex justify-center items-center`}>
+                    <Image
+                        src={"/not-found.png"}
+                        alt={"A cat with a confused face and question marks around it."}
+                        fill={true}
+                    />
+                </div>
+
+                <span className={`text-4xl font-bold`}>Page Not Found</span>
+            </div>
+
+        </div>
+    );
+}
