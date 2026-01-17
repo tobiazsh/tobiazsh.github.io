dc5e6dfe40b540c8f454b94c312f792798b1e8a3
Tobias <51285655+tobiazsh@users.noreply.github.com>
2026-01-17 14:18:57 +0100

    actually, not make funny 404 page because copyright

--- src/NotFoundPage.tsx
+++ src/NotFoundPage.tsx
@@ -7,10 +7,8 @@ const NotFoundPage = () => {
         <div style={{width:"100dvw", height:"100dvh", display:"flex", justifyContent:"center", alignItems:"center"}}>
             <div className={styles.container}>
                 <span className={styles.title_container}>
-                    Hey! So, that page doesn't exist ... or does it?
+                    Hey! So, that page doesn't exist...
                 </span>
-                <img src={"https://i.ytimg.com/vi/Q0NyKli3I-M/maxresdefault.jpg"} alt={"Michael Stevens (Vsauce) staring into the camera"}
-                     className={styles.vsauce_img}/>
                 <Link style={{textDecoration: "inherit", color: "inherit"}} to={"/"} className={styles.home_button}>
                     <AiOutlineArrowLeft className={styles.arrow}/>
                     Back to safety
