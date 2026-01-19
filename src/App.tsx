93198ec7d97bec76b18537a7a47a33136a43c734
Tobias <51285655+tobiazsh@users.noreply.github.com>
2026-01-19 13:13:46 +0100

    Lay foundation for website

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,23 @@
-import './App.css'
+import './App.module.css'
+import styles from "./App.module.css"
+import commons from "./styles/common.module.css"
+import lillyPfp from "/lilly.jpg"
 
 function App() {
 
     return (
-        <>
-            root
-        </>
+        <div style={{width: "100%", height: "100%", display: "flex", justifyContent: "center"}}>
+            <div className={[styles.main_card, commons.highlight_border].join(" ")}>
+                <span className={styles.title}>
+                    Hello, there!
+                </span>
+                <span className={styles.pfp}>
+                    <img src={lillyPfp}
+                         alt={"My cat lillyPfp: Yellow-ish eyes, white and fluffy fur with black spot on head. Looking into camera with open mouth and tilted head."}
+                         style={{width: "100%", height: "100%"}}/>
+                </span>
+            </div>
+        </div>
     )
 }
 
