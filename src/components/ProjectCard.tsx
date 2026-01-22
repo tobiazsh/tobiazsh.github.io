df725d1a386ff28f55ac863141476c5179c67bdb
Tobias <51285655+tobiazsh@users.noreply.github.com>
2026-01-22 13:21:50 +0100

    Update ProjectCard to use Link component for navigation and enhance styles

--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -1,26 +1,29 @@
 import commonStyles from '/src/styles/common.module.css';
 import styles from './ProjectCard.module.css';
+import {Link} from "react-router-dom";
 
 interface ProjectCardProps {
     projectName?: string;
     projectDescription?: string;
     imageUrl?: string;
+    clickUrl?: string;
 }
 
 const ProjectCard =
     ({
         projectName = "Sample Project",
         projectDescription = "A project that projects the project onto the project from the other team",
-        imageUrl
+        imageUrl,
+        clickUrl = ""
     }: ProjectCardProps) => {
     return (
-        <div className={[commonStyles.highlight_border, styles.card].join(' ')}>
+        <Link to={clickUrl} className={[commonStyles.highlight_border, styles.card].join(' ')}>
             <span className={styles.title}>{projectName}</span>
             <span>{projectDescription}</span>
             <span className={styles.img_container}>
                 <img src={imageUrl} alt={projectName} className={styles.project_image} />
             </span>
-        </div>
+        </Link>
     );
 }
 
