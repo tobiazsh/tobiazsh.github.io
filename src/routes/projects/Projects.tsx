4fe27833d99f8e87a5cccd3b0f811f4b4ff0d49a
Tobias <51285655+tobiazsh@users.noreply.github.com>
2026-01-22 11:56:49 +0100

    Add Projects Page

---
+++ src/routes/projects/Projects.tsx
@@ -0,0 +1,25 @@
+import ProjectCard from "../../components/ProjectCard.tsx";
+import lillyPfp from "/lilly.jpg";
+import styles from "./Projects.module.css";
+import HomeButton from "../../components/HomeButton.tsx";
+
+const Projects = () => {
+    return (
+        <div style={{width: "100dvw", display: "flex", alignItems: "center", flexDirection: "column"}}>
+            <span className={styles.home_button_container}>
+                <HomeButton />
+            </span>
+            <h1>My Projects</h1>
+            <div className={styles.projects_container}>
+                <ProjectCard imageUrl={lillyPfp}/>
+                <ProjectCard imageUrl={lillyPfp}/>
+                <ProjectCard imageUrl={lillyPfp}/>
+                <ProjectCard imageUrl={lillyPfp}/>
+                <ProjectCard imageUrl={lillyPfp}/>
+                <ProjectCard imageUrl={lillyPfp}/>
+            </div>
+        </div>
+    )
+}
+
+export default Projects;
