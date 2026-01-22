8744ca5959e8e432671031c0f770d74be2518228
Tobias <51285655+tobiazsh@users.noreply.github.com>
2026-01-22 08:24:21 +0100

    Create Contact Page

--- src/routes/contact/Contact.tsx
+++ src/routes/contact/Contact.tsx
@@ -1,8 +1,27 @@
+import commonStyles from '../../styles/common.module.css';
+import styles from './Contact.module.css';
+import {MdEmail} from "react-icons/md";
+import {BsDiscord} from "react-icons/bs";
+import HomeButton from "../../components/HomeButton.tsx";
+
 const Contact = () => {
     return (
-        <>
-            Contact!
-        </>
+        <div className={styles.container}>
+            <HomeButton style={{top: 0, left: 0, position: "absolute", margin: "20px"}}/>
+            <div className={[commonStyles.highlight_border, styles.card].join(' ')}>
+                Reach me at:
+                <span className={styles.method_container}>
+                    <a href={"mailto:developer.tobiazsh@gmail.com"}>
+                        <MdEmail />
+                        Email
+                    </a>
+                    <a href={"https://discord.com/users/566693559420452915"}>
+                        <BsDiscord />
+                        Discord
+                    </a>
+                </span>
+            </div>
+        </div>
     );
 }
 
